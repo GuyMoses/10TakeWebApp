f50c45bf8eaa74c36629b20106c8026a8af72c3c
Ido Ben-Natan <IdoBN@nimrod-bennatans-macbook-pro.local>
2014-11-13 18:41:23 +0200

    item's borrow btn calls function

--- www/src/10take.main/home/home.controller.js
+++ www/src/10take.main/home/home.controller.js
@@ -1,12 +1,17 @@
 (function() {
 
 	function HomeCtrl(log, stateParams, auth, items) {
+		var _this = this;
 		// log.debug('state params', stateParams);
 		// log.debug('auth', auth);
 		//this.name = stateParams.name;
 		this.items = items.all;
 		this.user = auth.user.name;
 		this.user = "Guy Moses";
+
+		_this.borrowClicked = function(arg1){
+			alert("Borrowed Item " + arg1);
+		};
 
 		log.debug('home ctrl auth', auth);
 		log.debug('home ctrl auth.user', auth.user);
