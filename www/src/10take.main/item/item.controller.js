605831d401ae6c1470f682066d1cc1061eb62437
Ido Ben-Natan <IdoBN@nimrod-bennatans-macbook-pro.local>
2014-11-13 16:24:28 +0200

    item's page additions

--- www/src/10take.main/item/item.controller.js
+++ www/src/10take.main/item/item.controller.js
@@ -1,10 +1,26 @@
 (function() {
 
-	function ItemCtrl(log, stateParams, auth, stateParams) {
+	function ItemCtrl(log, stateParams, auth, stateParams,items) {
 		this.id = stateParams.itemId;
+		log.debug(items);
+		this.all = items.all;
+
+		var doc = items.all;
+
+		var result = getById(doc, 1);
+
+		this.oneItem = result;
+
+		function getById(arr, id) {
+		    for (var d = 0, len = arr.length; d < len; d += 1) {
+		        if (arr[d].id === id) {
+		            return arr[d];
+		        }
+		    }
+		}
 	}
 
-		ItemCtrl.$inject = ['$log', '$stateParams', '$auth','$stateParams'];
+		ItemCtrl.$inject = ['$log', '$stateParams', '$auth','$stateParams','ItemsService'];
 
 	angular.module('10take.item')
 		.controller('ItemCtrl', ItemCtrl)
