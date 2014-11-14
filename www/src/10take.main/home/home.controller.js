78ae53fc4af03bee6d0e0eb263cda463885be6a9
Ido Ben-Natan <IdoBN@nimrod-bennatans-macbook-pro.local>
2014-11-14 03:31:27 +0200

    borrow's btn

--- www/src/10take.main/home/home.controller.js
+++ www/src/10take.main/home/home.controller.js
@@ -1,7 +1,8 @@
 (function() {
 
-	function HomeCtrl(log, stateParams, auth, items) {
+	function HomeCtrl(log, stateParams, auth, items, borrow) {
 		var _this = this;
+		this.showable = true;
 		this.user = auth.user.name;
 		this.user = "Guy Moses";
 		items.all()
@@ -11,13 +12,25 @@
 			})
 
 		_this.borrowClicked = function(arg1){
-			alert("Borrowed Item " + arg1);
+			borrow.newBorrow(arg1)
+				.success(function(data){
+					log.debug(data)
+					_this.showable = false;
+				})
 		};
+
+		_this.borrowDelete = function(arg1){
+			borrow.deleteBorrow(arg1)
+			.success(function(data){
+				log.debug(data)
+				_this.showable = true;
+			})
+		}
 
 		log.debug('home ctrl auth', auth);
 		log.debug('home ctrl auth.user', auth.user);
 	}
-		HomeCtrl.$inject = ['$log', '$stateParams', '$auth','ItemsService'];
+		HomeCtrl.$inject = ['$log', '$stateParams', '$auth','ItemsService','borrowService'];
 
 	angular.module('10take.home')
 		.controller('HomeCtrl', HomeCtrl)
---
+++ www/src/10take.services/borrow.service.js
@@ -0,0 +1,17 @@
+(function(){
+
+	function Borrow(http, URL, log){
+		this.newBorrow = function(item_id){
+			return http.post(URL+"/borrows",{item_id: item_id});
+		}
+
+		this.deleteBorrow = function(item_id){
+			return http.delete(URL+"/borrows/"+item_id)
+		}
+	}
+
+	Borrow.$inject = ['$http', 'URL', '$log'];
+
+	angular.module('10take.services')
+		.service('borrowService',Borrow)
+}());
